5d7cc91af13693b88bc265ba3d97c3b68bb1d612
Petrkudy <petrkudela94@seznam.cz>
2019-10-19 01:08:38 +0200

    views for display template

--- nic/views.py
+++ nic/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Domain, DomainFlag
 
-# Create your views here.
+
+class IndexListView(ListView):
+    template_name = 'index.html'
+    model = Domain
+
+    def get_context_data(self, **kwargs):
+        data = {'data': Domain.objects.filter(erdate=None)}
+        return data
+
+
+class DetailView(DetailView):
+    template_name = 'detail.html'
+    model = Domain
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['domain_flags'] = DomainFlag.objects.filter(domain=context['object'].id).exclude(flag='EXPIRED')
+        return context
